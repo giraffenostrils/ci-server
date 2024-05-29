db432d531c0bf792d931be999f5794e0aa4c2fa0
giraffenostrils <dreamthre@gamil.com>
2024-05-29 11:57:37 +0900

    aaa

--- src/main/java/com/example/smpoject/domain/SmRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.smpoject.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface SmRepository extends JpaRepository<Smploject,Long> {
-    List<Smploject> findByName(String name);
-}
